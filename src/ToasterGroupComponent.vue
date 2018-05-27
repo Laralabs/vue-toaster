7629cf850ca57093703314958e9370024cb521a5
Matt Clinton <matt@laralabs.uk>
2018-05-27 19:11:27 +0100

    Separate logic into different component

---
+++ src/ToasterGroupComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <notifications :group="group" :width="width" :position="position" :animation-type="animationType" :max="max" :reverse="reverse"></notifications>
+</template>
+<script>
+    const Component = {
+        name: 'ToasterGroup',
+        props: {
+            group: {
+                type: String,
+                default: ''
+            },
+            width: {
+                type: [Number, String],
+                default: 300
+            },
+            position: {
+                type: [String, Array],
+                default: 'top right'
+            },
+            animationType: {
+                type: String,
+                default: 'css',
+                validator (value) {
+                    return value === 'css' || value === 'velocity'
+                }
+            },
+            max: {
+                type: Number,
+                default: Infinity
+            },
+            reverse: {
+                type: Boolean,
+                default: false
+            }
+        }
+    };
+
+    export default Component
+</script>
--- src/ToasterLogicComponent.vue
+++ src/ToasterLogicComponent.vue
@@ -1,38 +1,8 @@
 <template>
-    <notifications :group="group" :width="width" :position="position" :animation-type="animationType" :max="max" :reverse="reverse"></notifications>
 </template>
 <script>
     const Component = {
-        name: 'Toaster',
-        props: {
-            group: {
-                type: String,
-                default: ''
-            },
-            width: {
-                type: [Number, String],
-                default: 300
-            },
-            position: {
-                type: [String, Array],
-                default: 'top right'
-            },
-            animationType: {
-                type: String,
-                default: 'css',
-                validator (value) {
-                    return value === 'css' || value === 'velocity'
-                }
-            },
-            max: {
-                type: Number,
-                default: Infinity
-            },
-            reverse: {
-                type: Boolean,
-                default: false
-            }
-        },
+        name: 'ToasterLogic',
         data() {
             return {
                 animation: {
